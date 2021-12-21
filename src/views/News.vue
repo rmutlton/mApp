d20e7e52c648bd803a3182fa873ff9d229110f17
Morkaot RMUTL <rmutlton@gmail.com>
2021-12-21 11:17:20 +0700

    Week 6

--- src/views/News.vue
+++ src/views/News.vue
@@ -6,23 +6,55 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">ข่าวสาร</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="ข่าวสาร" />
+      <ion-card>
+        <ion-item>
+          <ion-icon :icon="pin" slot="start"></ion-icon>
+          <ion-label>ข่าวสาร 1</ion-label>
+          <ion-button fill="outline" slot="end">View</ion-button>
+        </ion-item>
+
+        <ion-card-content>
+          This is content, without any paragraph or header tags,
+          within an ion-card-content element.
+        </ion-card-content>
+      </ion-card>
+      <ion-card>
+        <ion-item>
+          <ion-icon :icon="pin" slot="start"></ion-icon>
+          <ion-label>ข่าวสาร 2</ion-label>
+          <ion-button fill="outline" slot="end">View</ion-button>
+        </ion-item>
+
+        <ion-card-content>
+          This is content, without any paragraph or header tags,
+          within an ion-card-content element.
+        </ion-card-content>
+      </ion-card>
+      <ion-card>
+        <ion-item>
+          <ion-icon :icon="pin" slot="start"></ion-icon>
+          <ion-label>ข่าวสาร 3</ion-label>
+          <ion-button fill="outline" slot="end">View</ion-button>
+        </ion-item>
+
+        <ion-card-content>
+          This is content, without any paragraph or header tags,
+          within an ion-card-content element.
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '../components/ExploreContainer.vue';
+import { IonCard, IonCardContent, IonIcon, IonItem, IonLabel, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { pin } from 'ionicons/icons';
 
 export default  {
   name: 'News',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonCard, IonCardContent, IonIcon, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  setup() {
+    return { pin };
+  }
 }
 </script>
